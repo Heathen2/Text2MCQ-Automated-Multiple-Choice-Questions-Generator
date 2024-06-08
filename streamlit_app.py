29e5f3f7894bf3e3a129ffb8b3de919321303613
athul edassery <athulaugustine2@gmail.com>
2024-06-08 19:05:01 +0530

    updated download option

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from lang_xtract import get_quiz
 
-
 gpt_key = st.text_input("openai key")
 if gpt_key:
     file_to_process = st.file_uploader("Upload the file",type=["pdf"])
@@ -12,6 +11,7 @@ if gpt_key:
                 with open("input_to_process.pdf",'wb') as file:
                     file.write(file_to_process.getvalue())
                 response = get_quiz(gpt_key, "input_to_process.pdf")
+                st.session_state["response"] = response
             st.success("Extracted and saved")
             with open("Final_Quiz.xlsx", "rb") as file:
                 btn = st.download_button(
@@ -19,4 +19,6 @@ if gpt_key:
                 data=file,
                 file_name="Final_Quiz.xlsx",
                 )
-            st.write(response)
+            st.write(st.session_state["response"])
+        if "response" in st.session_state:
+            st.write(st.session_state["response"])    
